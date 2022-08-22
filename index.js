bca698ce30a6209ec4a8244ccd7acc1f6968b408
Bryan Wienhoff <106857564+Hoffalypse@users.noreply.github.com>
2022-08-21 21:29:13 -0600

    put all pages in place and started HTML Work

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const inquirer = require('inquirer');
 const jest = require('jest');
-const fs = require('fs')
+const fs = require('fs');
+const Eng = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager  = require('./lib/manager');
+const Employee = require('./lib/employee');
+
+
+//take in data from inquire(seperate pages or main)
+
+let myPromise = new Promise(function(resolve, reject) {
+    // "Producing Code" (May take some time)
+    
+      resolve(); // when successful
+      reject();  // when error
+    });
+    myPromise.then(
+        console.log(Eng)
+    )
